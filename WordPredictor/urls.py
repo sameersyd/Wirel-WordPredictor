63069071b6610ce46c8ef524e4403defef5b03fc
Sameer Nawaz <sameer.nwaz@gmail.com>
2023-01-28 11:14:26 -0800

    Test deploy (#1)
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Update requirements.txt
    
    * Update Allowed Host
    
    * Update allowed hosts
    
    * Update settings.py
    
    * Update path

--- WordPredictor/urls.py
+++ WordPredictor/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/word-predictor/v1/', include('api.urls')),
+    path('', include('api.urls')),
 ]
