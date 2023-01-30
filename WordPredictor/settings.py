66cbbc21e6ecab8c53d78047933d5531c98c2def
Sameer <sameer.nwaz@gmail.com>
2023-01-30 11:15:26 -0800

    Add django_on_heroku

--- WordPredictor/settings.py
+++ WordPredictor/settings.py
@@ -127,3 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
